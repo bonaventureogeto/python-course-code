4d7cd1deb4b5a3ebf6f76ace11f68f36104f0bff
Bonaventure Ogeto <bonaogeto1@gmail.com>
2025-04-16 23:49:58 +0300

    add decision making in python with if-else statements

---
+++ if-else.py
@@ -0,0 +1,18 @@
+# age = 20
+
+# if age >= 18:
+#     print("You are an adult.")
+# else:
+#     print("You are underage.")
+
+# grading system
+score = 40
+
+if score >= 70:
+    print("Grade A")
+elif score >= 60:
+    print("Grade B")
+elif score >= 50:
+    print("Grade C")
+else:
+    print("Grade of D or Lower")
